922e917667a87329faca777b7b6cc7db06b876e7
Ashfaq <ashfaq.abdi@gmail.com>
2024-01-03 19:39:04 +0300

    init app via vite

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1>OTTOMAN</h1>
+      
+      
+    </>
+  )
+}
+
+export default App
